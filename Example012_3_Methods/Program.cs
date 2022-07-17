41266b6d0e635247de8e3a3ae394c19630ef9e43
galetskaya <li-gn@yandex.ru>
2022-07-17 15:18:57 +0200

    Sort from max to min

---
+++ Example012_3_Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿// Нужно отсортировать элементы массива от наименьшего к наибольшему
+
+int[] arr = { 10, 5, 14, 3, 2, 6, 7, 11, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition])
+            {
+                maxPosition = j;
+            }
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
